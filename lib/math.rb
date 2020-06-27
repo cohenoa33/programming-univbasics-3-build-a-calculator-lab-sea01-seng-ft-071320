d1a36586d2f7e224886d578d8ff2a013b342ea85
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-06-27 21:53:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ sum
 end
 
 def subtraction(num1, num2)
-
+sum = num1 - num2
+sum
 end
 
 def division(num1, num2)
-
+sum = num2 * num1
+sum
 end
 
 def multiplication(num1, num2)
-
+sum = num2 / num1
+sum
 end
 
 def modulo(num1, num2)
