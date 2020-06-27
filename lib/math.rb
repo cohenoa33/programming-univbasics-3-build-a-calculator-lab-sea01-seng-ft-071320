09c7c9ba108d412d4e06073d0daee95905a84e59
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-06-27 21:57:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,5 @@
 def addition(num1, num2)
-sum = num2 + num1
+sum = num1+ num2
 sum
 end
 
@@ -14,14 +14,16 @@ sum
 end
 
 def multiplication(num1, num2)
-sum = num2 * num1
+sum = num1 * num2
 sum
 end
 
 def modulo(num1, num2)
-
+sum = num1 % num2
+sum
 end
 
 def square_root(num)
-
+sum = num1 ** num2
+sum
 end
